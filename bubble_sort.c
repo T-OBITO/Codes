2a048b56499c274bb07c26a51d20e65ab60dd69a
Sheershak sharma <98447782+T-OBITO@users.noreply.github.com>
2023-12-11 23:51:41 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,6 +1,11 @@
+
 #include <stdio.h>
 int main(){
     int arr[7]={34,56,4,3,21,5,6},temp;
+    printf("The array is:\n");
+    for (int i=0;i<7;i++){
+        printf("\t%d,",arr[i]);
+    }
     for (int i=0;i<7;i++){
         for (int j=0;j<6;j++){
             if (arr[i]<arr[j]){
@@ -13,13 +18,11 @@ int main(){
             }
         }
     }
-
+    printf("\nThe sorted array is:\n");
     for (int i=0;i<7;i++){
         printf("\t%d,",arr[i]);
     }
     return 0;
 }
-
-
 
 
