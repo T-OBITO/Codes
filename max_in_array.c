4b7498de1382748ed83f34ba8a9bdf8c744be6eb
Sheershak sharma <98447782+T-OBITO@users.noreply.github.com>
2023-12-11 22:34:11 +0530

    Update max_in_array.c

--- max_in_array.c
+++ max_in_array.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(){
-    int arr[6]={1,26,31,4,5,6},max;
+    int arr[6]={1,26,31,4,5,6},max,indx;
     max=arr[3];
     for (int i=0;i<6;i++){
         if (max<arr[i]){
             max=arr[i];
+            indx=i;
         }
     }
-    printf("Maximum number from the array= %d",max);
+    printf("Maximum number from the array= %d at %d",max,indx);
     return 0;
 }
