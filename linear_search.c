5af98f1d6903237ce9e0c6bad48535a1ebb673b1
Sheershak sharma <98447782+T-OBITO@users.noreply.github.com>
2023-12-11 23:48:33 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main (){
-    int arr[9]={23,56,78,65,35,90,31,87,56},num;
+    int arr[9]={23,56,78,65,35,90,31,87,56},num,flag=0;
     printf("Enter the number you want to search :");
     scanf("%d",&num);
     for (int i=0;i<9;i++){
         if (arr[i]==num){
             printf("Number found at %d position\n",i+1);
+            flag=1;
         }
+    }
+    if (flag==0){
+        printf("Number not found\n");
     }
     return 0;
 }
